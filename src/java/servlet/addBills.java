1e42f5623919ffb53f38de26d365a9f8a145cb0c
eabiii <edbar_10@yahoo.com>
2017-11-24 19:18:02 +0800

    Added add bill servlet

---
+++ src/java/servlet/addBills.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlet;
+
+import dao.BillingDao;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Billing;
+
+/**
+ *
+ * @author eabiii
+ */
+@WebServlet(name = "addBills", urlPatterns = {"/addBills"})
+public class addBills extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        int billID=Integer.parseInt(request.getParameter("billID"));
+        int blockNum=Integer.parseInt(request.getParameter("blockNum"));
+        int lotNum=Integer.parseInt(request.getParameter("lotNum"));
+        double totalPaid=Double.parseDouble(request.getParameter("totalPaid"));
+        double totalDue=Double.parseDouble(request.getParameter("totalDue"));
+        Billing bill=new Billing(billID,blockNum,lotNum,billID,totalPaid,totalDue);
+        if(BillingDao.addNewBill(bill))
+        {
+            request.getRequestDispatcher("FA_BillCo_Documents_DefaultPage.jsp").forward(request, response);
+        }
+        
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
